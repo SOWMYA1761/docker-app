c4c64f930fb2133a637dfd1a42edfb2deaa82519
Sowmya Chinthamaneni <51991807+SOWMYA1761@users.noreply.github.com>
2021-10-17 13:23:41 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -1,7 +1,7 @@
-class Add{  
-public static void main(String[] args){ 
+class Add{ 
 private int a = 10;
 private int b = 20;
+public static void main(String[] args){
 System.out.println("This is java app \n by using Docker and the sum is = " + (a+b));  
 }  
 }  
